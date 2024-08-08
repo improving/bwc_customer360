734e1f0e188390d3b3b5d9938b080445ccc4e9ad
Nick Larson <nicklas.larson@gmail.com>
2024-08-08 12:55:54 -0500

    cleanup

--- template/modules/product_team_avro_topic/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "topic" {
-  description = "The Kafka Topic that was provisioned in the provided cluster"
-  value       = confluent_kafka_topic.topic
-}
-
-output "topic_value_schema" {
-  description = "The Kafka Topic that was provisioned in the provided cluster"
-  value       = confluent_schema.value_schema
-}
--- template/shadowtraffic/customers/start-kafka.sh
+++
@@ -1,8 +0,0 @@
-docker run \
-  --pull=always \
-  --env-file ../license.env \
-  -v $(pwd)/config:/config \
-  -v $(pwd)/../../kafka-product-team-customer/staging/schemas:/schemas \
-  shadowtraffic/shadowtraffic:latest \
-  --config /config/config-avro.json \
-  --watch
